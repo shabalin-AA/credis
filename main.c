11d95b154e3d3278253b4ee3488737ff3c677527
Shabalin Aleksei <shabalin0704@outlook.com>
2024-06-26 21:54:08 +0500

    empty main

--- main.c
+++ main.c
@@ -8,19 +8,5 @@
 
 int main( void )
 {
-  Set set = {0};
-  set_insert(&set, value_int(1));
-  set_insert(&set, value_int(2));
-  set_insert(&set, value_int(1));
-  set_insert(&set, value_int(3));
-  set_insert(&set, value_int(4));
-  for (size_t i = 0; i < set.length; i++) {
-    printf("%d ", set.items[i].integer);
-  } printf("\n");
-  printf("%zd\n", set_indexof(&set, value_int(5)));
-  set_remove(&set, set_indexof(&set, value_int(5)));
-  for (size_t i = 0; i < set.length; i++) {
-    printf("%d ", set.items[i].integer);
-  } printf("\n");
   return 0;
 }
