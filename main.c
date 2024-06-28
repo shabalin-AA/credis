95e8b0091adb7544f658bf2add8a59e1ffd133ae
Shabalin Aleksei <shabalin0704@outlook.com>
2024-06-28 18:49:40 +0500

    server

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <assert.h>
 #include <stdlib.h>
 #include <string.h>
 #include "value.h"
@@ -7,29 +8,58 @@
 #include "set.h"
 #include "hashtable.h"
 
-int main( void )
+#include <sys/socket.h>
+#include <sys/types.h> 
+#include <netinet/in.h> 
+#include <unistd.h>
+#include <pthread.h>
+
+typedef struct sockaddr_in sockaddr_in;
+typedef struct sockaddr sockaddr;
+
+void* handle_client(void* arg)
 {
-  Hashtable table = {0};
-  ht_insert(&table, str_new("key1"), value_int(1));
-  ht_insert(&table, str_new("key2"), value_int(2));
-  ht_insert(&table, str_new("key3"), value_int(3));
-  ht_insert(&table, str_new("key3"), value_int(33));
-  ht_insert(&table, str_new("key4"), value_int(4));
-  ht_insert(&table, str_new("key5"), value_int(5));
-  ht_insert(&table, str_new("key6"), value_int(6));
-  ht_insert(&table, str_new("key7"), value_int(7));
-  for (size_t i = 0; i < table.capacity; i++) {
-    Pair item = table.items[i];
-    if (item.key) {
-      printf("%d; %s: %d\n", i, item.key, integer_value(left(&item.chain)->value));
+  int client_fd = *(int*)arg;
+  char buf[256];
+  while (1) {
+    int len = read(client_fd, buf, sizeof(buf));
+    buf[len-1] = 0;
+    printf("%s\n", buf);
+    if (strcmp(buf, "quit") == 0) {
+      close(client_fd);
+      break;
     }
   }
-  ht_remove(&table, str_new("key3"));
-  for (size_t i = 0; i < table.capacity; i++) {
-    Pair item = table.items[i];
-    if (item.key) {
-      printf("%d; %s: %d\n", i, item.key, integer_value(left(&item.chain)->value));
-    }
+  return NULL;
+}
+
+int main(void)
+{
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  assert(sockfd > 0 && "socket() failed");
+  
+  sockaddr_in server_addr = {0};
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  server_addr.sin_port = htons(6399);
+  
+  int bind_res = bind(sockfd, (sockaddr*)&server_addr, sizeof(server_addr));
+  assert(bind_res == 0 && "bind() failed");
+  
+  int listen_res = listen(sockfd, 4);
+  assert(listen_res == 0 && "listen() failed");
+  
+  sockaddr_in client_addr;
+  socklen_t client_addr_len;
+  
+  while (1) {
+    int clientfd = accept(sockfd, (sockaddr*)&client_addr, &client_addr_len);
+    assert(clientfd > 0 && "accept() failed");
+    printf("%d connected\n", clientfd);
+    pthread_t thread;
+    pthread_create(&thread, NULL, handle_client, &clientfd);
   }
+  
+  close(sockfd);
   return 0;
 }
